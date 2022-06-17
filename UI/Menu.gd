c3a840110f507baef15704acbd781c48a89a51b4
Craiky <31390334+Craiky@users.noreply.github.com>
2022-06-17 10:31:07 -0700

    Fixed menu

--- UI/Menu.gd
+++ UI/Menu.gd
@@ -8,7 +8,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Button2.grab_focus()
+	$TextureButton.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,11 +16,6 @@ func _ready():
 #	pass
 
 
-func _on_Button2_pressed():
+
+func _on_TextureButton_pressed():
 	get_tree().change_scene("res://MainScene.tscn")
-	
-
-
-
-func _on_Button_pressed():
-	get_tree().quit()
